318c0146ee59d16c45e584bb06ed8ea622f9114f
flashdrag <flashdrag@gmail.com>
2023-01-04 15:24:14 +0000

    insert random letters to the wheel by interval and than Run functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,40 @@
 let but = document.getElementsByTagName('button')[0]
-but.addEventListener('click', insertWord)
-but.addEventListener('click', displayDefinition)
+but.addEventListener('click', runGame)
 
 /**
- * Displays the word definition to the user
+ * Insert random letters to the wheel by interval and than Run functions
  */
-function displayDefinition() {
-  // let definition = getDefinition() //the func returns recieved definition from api and store in var
-  let definition = 'Annual native to South America having underground stolons bearing edible starchy tubers; widely cultivated as a garden vegetable; vines are poisonous'
+function runGame() {
+  let charWindow = document.getElementById('wheel-letter');
+  let timer = setInterval(insertRandomChar, 100); // insert random letters to the wheel by interval
+
+  // Set a delay for running functions
+  setTimeout(function() {
+    // let word = getRandomWord();  // store the recieved word to the var
+    // let definition = getDefinition(); // store the recieved definition to the var
+    let word = 'potato';
+    let definition = 'Annual native to South America having underground stolons bearing edible starchy tubers; widely cultivated as a garden vegetable; vines are poisonous'
+    displayDefinition(definition);  // run displayDefinition func with passed definition
+    insertWord(word);  // run insertWord func with passed word
+    clearInterval(timer);  // stop changing random letters in the wheel
+    charWindow.innerHTML = "?";
+  }, 1000)
+}
+
+/**
+ * Insert random char to the wheel
+ */
+function insertRandomChar() {
+  let wheel = document.getElementById('wheel-letter');
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let ranNum = Math.floor(Math.random() * chars.length);  //generate random num from 0 to 'chars' string length
+  wheel.innerHTML = chars[ranNum];  // get random char from the 'chars' string and insert to the 'wheel' element
+};
+
+/**
+ * Get and Display the word definition to the user
+ */
+function displayDefinition(definition) {
   let definitionEl = document.getElementById('definition');
   let definitionWrapper = document.getElementById('definition-wrapper');
 
@@ -20,9 +47,7 @@ function displayDefinition() {
  * Get and Divide the correct word into flip cards for each letter
  * and show them to the user with the reverse sider
  */
-function insertWord() {
-  let word = 'potato';
-  // let word = getRandomWord();  // store the recieved word to the var
+function insertWord(word) {
   let wordListEl = document.getElementById('word-list');
 
   for (char of word) {
@@ -36,8 +61,7 @@ function insertWord() {
     wordListEl.style.background = 'none';
 
     li.addEventListener('click', showLetter);
-  }
-  console.log(wordListEl)
+  };
 }
 
 
